067906ef684cab96c019ca8955ec8e897fd844e1
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-10-04 02:51:15 +0600

    check

--- js/index.js
+++ js/index.js
@@ -83,3 +83,44 @@ const emptyInput =document.getElementById("empty-input");
 
 const searchBtn =document.getElementById("search-button");
 
+document.getElementById("search-city").addEventListener("keypress",(event) => {
+    if(event.key === "Enter"){
+        searchBtn.click();
+    }
+});
+
+//2nd kaj & load weather URL
+const loadSearch = async (city) => {
+    const api = "73894e9249ceb0a8ce39173aec61e91f";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={api}&units=metric`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayWeather(data);
+}
+
+//3rd kaj & Display weather
+const displayWeather = (temparature) => {
+    //console.log(temparature);
+    if (temparature.message === "city not found") {
+        emptyInput.innerHTML = `
+            <h4 class="text-start text-danger mt-2">No results found!</h4>
+        `;
+    }
+    const container = document.getElementById("container");
+    container.textContent = "";
+    const localDate = convertUnixTimeToLocal(temparature.dt);
+    const sunriseTime = convertUnixTimeToLocal(temparature.sys.sunrise);
+    const sunsetTime = convertUnixTimeToLocal(temparature.sys.sunset);
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <h4 class="fs-2">${temparature.name}, ${temparature.sys.country}</h4>
+        <h6>${localDate.fullDate}</h6>
+        <img src="http://openweathermap.org/img/wn/${temparature.weather[0].icon}@2x.png" alt="">
+        <h5 class="fs-1">${temparature.main.temp} &deg;C</h5>
+        <h5>${temparature.weather[0].main}</h5>
+        <h5><span class="me-3">Sunrise: ${sunriseTime.time12h}</span> & <span class="ms-3> Sunset: ${sunsetTime.time12h}</span></h5>`;
+        container.appendChild(div);
+        
+}
+
+loadSearch("Dhaka");
